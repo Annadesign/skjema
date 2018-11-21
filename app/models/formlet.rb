4fabd0f17eeff88243aba7cfa84cfafde53a1fcf
annadesign <konto@annadesign.no>
2018-11-21 23:25:49 +0100

    fix

--- app/models/formlet.rb
+++ app/models/formlet.rb
@@ -1,12 +1,6 @@
 class Formlet < ApplicationRecord
 	validates :name, presence: true
   	validates :email, presence: true
-  	validates :company, presence: true
-  	validates :position, presence: true
-  	validates :duration, presence: true 
-  	validates :challenge, presence: true 
-  	validates :motivation, presence: true
-  	validates :grounds, presence: true	
   	validates :document, presence: true
 
   	mount_uploader :document, DocumentUploader
